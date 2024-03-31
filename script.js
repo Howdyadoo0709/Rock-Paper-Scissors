a34c6c31ab07052d366803767cba34a9fcbf53b4
Loay <loay.gspg@gmail.com>
2024-03-31 13:39:23 -0500

    Added if/else statement for a single round of rock paper scissors

--- script.js
+++ script.js
@@ -6,5 +6,36 @@ function getComputerChoice() {
 }
 
 function playerSelection() {
-    const playerChoice = prompt("Please choose Rock, Paper, or Scissors.")
+    let playerAsk = prompt("Please choose Rock, Paper, or Scissors");
+    console.log(playerAsk);
+}
+
+function playRound (playerSelection, getComputerChoice) {
+    if (playerSelection === 'Rock' && computerRandom === 'Rock') {
+        return "Draw!";
+    }
+    if (playerSelection === 'Rock' && computerRandom === 'Scissors') {
+        return "Win!";
+    }
+    if (playerSelection === 'Rock' && computerRandom === 'Paper') {
+        return "Lose!";
+    }
+    if (playerSelection === 'Paper' && computerRandom === 'Rock') {
+        return "Win!";
+    }
+    if (playerSelection === 'Paper' && computerRandom === 'Paper') {
+        return "Draw!";
+    }
+    if (playerSelection === 'Paper' && computerRandom === 'Scissors') {
+        return "Lose!";
+    }
+    if (playerSelection === 'Scissors' && computerRandom === 'Scissors') {
+        return "Draw!";
+    }
+    if (playerSelection === 'Scissors' && computerRandom === 'Rock') {
+        return "Lose!";
+    }
+    if (playerSelection === 'Scissors' && computerRandom === 'Paper') {
+        return "Win!";
+    }
 }
